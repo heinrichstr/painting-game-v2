ad777e08775f2c3cb219b13b6936979a4b8ed711
Thomas Heinrichs <tom.heinri@gmail.com>
2022-04-16 16:43:25 -0400

    update inventory text, update inventory button UI, alias button font, animate button hover, begin creating shader for UI panel

--- scenes/Button.gd
+++ scenes/Button.gd
@@ -1,8 +1,9 @@
 extends Button
 
-var font = load('res://assets/fonts/dancing_script.tres')
-var myString = "pill bottle"
-var x = (110) / myString.length()
+var font = load('res://assets/fonts/roboto_slab.tres')
+
+export var buttonString = "pill bottle"
+var x = (110) / buttonString.length()
 var showText = false
 
 # Called when the node enters the scene tree for the first time.
@@ -11,14 +12,21 @@ func _ready():
 
 func _on_Button_mouse_entered():
 	showText = true
+	$"../AnimatedSprite".rotation_degrees = -15
 	update()
 
 func _on_Button_mouse_exited():
 	showText = false
+	$"../AnimatedSprite".rotation_degrees = 0
 	update()
 
 func _draw(): #draws text in a circle above the icon on hover
 	if showText:
-		for i in range(0, myString.length()):
-			draw_set_transform(Vector2(50, 50), deg2rad(i * x - (myString.length()/2*x)), Vector2(1,1))
-			draw_char(font, Vector2(-5, -60), myString[i], "", Color(0, 0, 0))
+		for i in range(0, buttonString.length()):
+			draw_set_transform(Vector2(50, 50), deg2rad(i * x - (buttonString.length()/2*x)), Vector2(1,1))
+			draw_char(font, Vector2(-5, -60), buttonString[i], "", Color(0, 0, 0))
+
+
+func _on_Button_pressed():
+	print(buttonString, "--button pressed!")
+
